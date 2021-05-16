d3e6902eb4d93e7c66a7c0b4fbbca5294a16c890
JHM <JHM@DESKTOP-IGR373E>
2021-05-16 16:46:20 +0900

    Hello

--- src/Hello2/testm.java
+++ src/Hello2/testm.java
@@ -1,5 +1,8 @@
 package Hello2;
 
-public class testm
-{public static void main(String[] args)
-{ System.out.print("test");} }
+public class testm {
+	public static void main(String[] args){
+		
+	System.out.print("test");
+	}
+}
